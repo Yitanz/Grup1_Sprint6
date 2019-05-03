2c81aa02fbcba3178e20672484956cc3d86fa29f
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2019-05-03 16:31:13 +0200

    Update 2019_01_22_180830_create_users_table.php

--- database/migrations/2019_01_22_180830_create_users_table.php
+++ database/migrations/2019_01_22_180830_create_users_table.php
@@ -34,7 +34,6 @@ class CreateUsersTable extends Migration
             $table->foreign('id_rol')->references('id')->on('rols');
             $table->unsignedInteger('id_dades_empleat')->nullable();
             $table->foreign('id_dades_empleat')->references('id')->on('dades_empleats');
-            $table->boolean('actiu');
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
